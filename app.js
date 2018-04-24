c9fa594d3002feb5c53aab172e481992886ac1e0
muxiao <540690458@qq.com>
2018-04-24 23:58:27 +0800

    2

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ var path = require('path')
 var port = process.env.PORT || 3000;
 var app = express();
 var mongoose = require('mongoose');
+var mongoStore = require('connect-mongo')(express);
 var Movie = require('./models/movie');
+var User = require('./models/user');
 var moment = require('moment');
-var _ = require('underscore')
+var _ = require('underscore');
+var dbUrl = 'mongodb://localhost/imooc';
+
+mongoose.connect(dbUrl);
 
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
+app.use(express.cookieParse())
+app.use(express.session({
+    secrect: 'imooc',
+    store: new mongoStore({
+        url: dbUrl,
+        collection: 'sessions'
+    })
+}))
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/', express.static('./node_modules'));
@@ -23,6 +36,17 @@ console.log('服务启动成功:' + port);
 mongoose.connect('mongodb://localhost/imooc', function () {
     console.log('数据库连接成功');
 });
+
+// pre handle user
+app.use((req, res, next) => {
+    var _user = req.session.user;
+    
+    if(_user) {
+        app.locals.user = _user;
+    }
+
+    return next();
+})
 
 app.get('/', function (req, res) {
     Movie.fetch(function (err, movies) {
@@ -144,3 +168,82 @@ app.delete('/admin/list', function(req, res) {
         })
     }
 })
+
+// signin
+app.post('/user/signup', (req, res) => {
+    var _user = req.body.user;
+
+    User.find({name: _user.name}, (err, user) => {
+        if(err) {
+            console.log(err)
+        }
+
+        if(user) {
+            return res.redirect('/');
+        } else {
+            var user = new User(_user);
+
+            user.save((err, user) => {
+                if(err) {
+                    console.loga(err);
+                }
+        
+                res.redirect('/admin/userlist')
+            })
+        }
+    })
+})
+
+// userlist page
+app.get('/admin/userlist', function (req, res) {
+    User.fetch(function (err, users) {
+        if (err) {
+            console.log(err)
+        }
+
+        res.render('userlist', {
+            title: 'imooc 用户列表页',
+            users,
+        })
+    })
+})
+
+// signin
+app.post('/user/signin', (req, res) => {
+    let _user = req.body.user,
+        name = _user.name,
+        password = _user.password;
+
+    User.findOne({name: name}, (err, user) => {
+        if(err) {
+            console.log(err);
+        }
+
+        if(!user) {
+            return res.direct('/');
+        }
+
+        user.comparePassword(password, (err, isMatch) => {
+            if(err) {
+                console.log(err);
+            }
+
+            if(isMatch) {
+                req.session.user = user;
+
+                return res.redirect('/');
+            } else {
+                console.log('password is not matched');
+            }
+        })
+    })
+})
+
+// logout
+app.get('/logout', (req, res) => {
+    delete req.session.user;
+    delete app.locals.user;
+
+    res.direct('/');
+})
+
---
+++ schemas/user.js
@@ -0,0 +1,74 @@
+// 模式 对数据字段进行定义
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+var UserSchema = new mongoose.Schema({
+    name: {
+        unique: true,
+        type: String
+    },
+    password: String,
+    meta: {
+        createAt: {
+            type: Date,
+            default: Date.now()
+        },
+        updateAt: {
+            type: Date,
+            default: Date.now()
+        }
+    },
+})
+
+// 存数据之前调用
+UserSchema.pre('save', function(next) {
+    var user = this;
+    
+    if(this.isNew) {
+        this.meta.createAt = this.meta.updateAt = Date.now();
+    } else {
+        this.meta.updateAt = Date.now();
+    }
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if(err) {
+            return next(err)
+        }
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if(err) {
+                return next(err);
+            }
+
+            user.password = hash;
+        })
+    })
+
+    next();
+})
+
+UserSchema.methods = {
+    comparePassword(_password, cb) {
+        bcrypt.compare(_password, this.password, (err, isMatch) => {
+            if(err) {
+                return cb(err);
+            }
+
+            cb(null, isMatch);
+        })
+    }
+}
+
+// 模式的静态方法 需要模型编译起效
+UserSchema.statics = {
+    fetch: function(cb) {
+        return this.find({}, cb)
+    },
+    findById: function(id, cb) {
+        return this.findOne({_id: id}, cb);
+    }
+}
+
+module.exports = UserSchema; // 导出模式
